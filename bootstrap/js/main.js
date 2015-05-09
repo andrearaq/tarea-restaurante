73d3dfdc0b383241e8418b88eec5bf344dcf918b
andrea <andrearaq@hotmail.com>
2015-05-10 01:24:46 +0200

    fotos en carpetas

--- bootstrap/js/main.js
+++ bootstrap/js/main.js
@@ -48,6 +48,44 @@ $('#localizacion').click(function(){
         //    $('#cabecera2').show();
            $('#contenedor').load('localizacion.html');
     });
+
+$('#entrantes').click(function(){
+          //  $(this).parent('li').prop('class','active');
+        //    $('#li_inicio, #li_localizacion, #li_menu, #li_mesa, #li_platos').prop('class','');
+         //   $('#cabecera').hide();
+        //    $('#cabecera2').show();
+           $('#contenedor').load('entremeses.html');
+    });
+
+$('#primeros').click(function(){
+          //  $(this).parent('li').prop('class','active');
+        //    $('#li_inicio, #li_localizacion, #li_menu, #li_mesa, #li_platos').prop('class','');
+         //   $('#cabecera').hide();
+        //    $('#cabecera2').show();
+           $('#contenedor').load('primeros.html');
+    });
+
+$('#segundos').click(function(){
+          //  $(this).parent('li').prop('class','active');
+        //    $('#li_inicio, #li_localizacion, #li_menu, #li_mesa, #li_platos').prop('class','');
+         //   $('#cabecera').hide();
+        //    $('#cabecera2').show();
+           $('#contenedor').load('segundos.html');
+    });
+$('#postres').click(function(){
+          //  $(this).parent('li').prop('class','active');
+        //    $('#li_inicio, #li_localizacion, #li_menu, #li_mesa, #li_platos').prop('class','');
+         //   $('#cabecera').hide();
+        //    $('#cabecera2').show();
+           $('#contenedor').load('postres.html');
+    });
+$('#bebidas').click(function(){
+          //  $(this).parent('li').prop('class','active');
+        //    $('#li_inicio, #li_localizacion, #li_menu, #li_mesa, #li_platos').prop('class','');
+         //   $('#cabecera').hide();
+        //    $('#cabecera2').show();
+           $('#contenedor').load('bebidas.html');
+    });
 //});
         
  
