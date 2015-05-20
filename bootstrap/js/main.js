9d8eec3440de79c180a35a2f58746c79f70bcb3f
andrea <andrearaq@hotmail.com>
2015-05-21 01:42:25 +0200

    falta validacion

--- bootstrap/js/main.js
+++ bootstrap/js/main.js
@@ -8,69 +8,62 @@
 // opciones del menú
 
      $('#inicio').click(function(){
-          //  $(this).parent('li').prop('class','active');
-        //    $('#li_inicio, #li_localizacion, #li_menu, #li_mesa, #li_platos').prop('class','');
+        //   $(this).parent('li').prop('class','active');
+        //   $('#localizacion, #menus, #reservas, #galeria, #chef').prop('class','');
            $('#contenedor').load('inicio.html');
     });
     $('#menus').click(function(){
-            $(this).parent('li').prop('class','active');
-        //    $('#li_inicio, #li_localizacion, #li_menu, #li_mesa, #li_platos').prop('class','');
+      //      $(this).parent('li').prop('class','active');
+    //    $('#inicio, #localizacion, #reservas, #galeria, #chef').prop('class','');
            $('#contenedor').load('menus.html');
     });
     $('#galeria').click(function(){
-          //  $(this).parent('li').prop('class','active');
-        //    $('#li_inicio, #li_localizacion, #li_menu, #li_mesa, #li_platos').prop('class','');
+        //   $(this).parent('li').prop('class','active');
+        //$('#inicio, #localizacion, #menus, #reservas #chef').prop('class','');
            $('#contenedor').load('galeria.html');
     });
     $('#reservas').click(function(){
-          //  $(this).parent('li').prop('class','active');
-        //    $('#li_inicio, #li_localizacion, #li_menu, #li_mesa, #li_platos').prop('class','');
+       //     $(this).parent('li').prop('class','active');
+     //   $('#inicio, #localizacion, #menus, #galeria, #chef').prop('class','');
            $('#contenedor').load('reservas.html');
     });
     $('#localizacion').click(function(){
           //  $(this).parent('li').prop('class','active');
-        //    $('#li_inicio, #li_localizacion, #li_menu, #li_mesa, #li_platos').prop('class','');
+        //$('#inicio, #menus, #reservas, #galeria, #chef').prop('class','');
            $('#contenedor').load('localizacion.html');
     });
     $('#chef').click(function(){
-          //  $(this).parent('li').prop('class','active');
-        //    $('#li_inicio, #li_localizacion, #li_menu, #li_mesa, #li_platos').prop('class','');
+      //      $(this).parent('li').prop('class','active');
+    //    $('#inicio, #localizacion, #menus, #reservas, #galeria, #chef').prop('class','');
            $('#contenedor').load('chef.html');
     });
     $('#entrantes').click(function(){
-          //  $(this).parent('li').prop('class','active');
-        //    $('#li_inicio, #li_localizacion, #li_menu, #li_mesa, #li_platos').prop('class','');
-         //   $('#cabecera').hide();
-        //    $('#cabecera2').show();
+      //      $(this).parent('li').prop('class','active');
+    //    $('#inicio, #localizacion, #menus, #reservas, #galeria, #chef').prop('class','');
            $('#contenedor').load('entremeses.html');
     });
 
     $('#primeros').click(function(){
           //  $(this).parent('li').prop('class','active');
-        //    $('#li_inicio, #li_localizacion, #li_menu, #li_mesa, #li_platos').prop('class','');
+        //$('#inicio, #localizacion, #menus, #reserva, #galeria', 'chef').prop('class','');
            $('#contenedor').load('primeros.html');
     });
 
     $('#segundos').click(function(){
-              //  $(this).parent('li').prop('class','active');
-            //    $('#li_inicio, #li_localizacion, #li_menu, #li_mesa, #li_platos').prop('class','');
+        // $(this).parent('li').prop('class','active');
+        //  $('#inicio, #localizacion, #menus, #reservas, #galeria, #chef').prop('class','');
                $('#contenedor').load('segundos.html');
         });
     $('#postres').click(function(){
-              //  $(this).parent('li').prop('class','active');
-            //    $('#li_inicio, #li_localizacion, #li_menu, #li_mesa, #li_platos').prop('class','');
-             //   $('#cabecera').hide();
-            //    $('#cabecera2').show();
+        //        $(this).parent('li').prop('class','active');
+        //   $('#inicio, #localizacion, #menus, #reservas, #galeria, #chef').prop('class','');
                $('#contenedor').load('postres.html');
         });
     $('#bebidas').click(function(){
-              //  $(this).parent('li').prop('class','active');
-            //    $('#li_inicio, #li_localizacion, #li_menu, #li_mesa, #li_platos').prop('class','');
+        //      $(this).parent('li').prop('class','active');
+        //   $('#inicio, #localizacion, #menus, #reservas, #galeria, #chef').prop('class','');
                $('#contenedor').load('bebidas.html');
-       //$('#contenedor').load('chef.html');
         });
 
-// codigo para la galeria de imagenes
-  /*  $('#contenedor').on('click','.gal', function(){                                                                            $                   $('#largeImage').attr('src',$(this).attr('src'));
-    });*/
+
  
